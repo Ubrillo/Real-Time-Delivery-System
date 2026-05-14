7d529f5dae6e744e7d161f461427af8eb1b4653e
Ubrillo <ubrillo@gmail.com>
2026-05-14 18:03:43 +0100

    dispatcher queue class v1

--- src/main/java/com/ubrillo/ubrillodeliverysystem/Logic/DispatchQueue.java
+++ src/main/java/com/ubrillo/ubrillodeliverysystem/Logic/DispatchQueue.java
@@ -1,4 +1,44 @@
 package com.ubrillo.ubrillodeliverysystem.Logic;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class DispatchQueue {
+    private Queue<Request> mainQueue;
+    private Queue<Request> queueNorthLondon;
+    private Queue<Request> queueCentralLondon;
+    private Queue<Request> queueWestLondon;
+    private Queue<Request> queueEastLondon;
+    private Queue<Request> queueSouthLondon;
+    private Zone zone;
+
+    public DispatchQueue(){
+        mainQueue = new LinkedList<>();
+        queueSouthLondon = new LinkedList<>();
+        queueCentralLondon = new LinkedList<>();
+        queueNorthLondon = new LinkedList<>();
+        queueEastLondon = new LinkedList<>();
+    }
+
+    public void addOrder(Request order){
+        mainQueue.add(order);
+    }
+
+    public Request getOrder(){
+        return mainQueue.poll();
+    }
+
+    public void sorter(){
+        while (!mainQueue.isEmpty()){
+            Request order = mainQueue.poll();
+            //findZone(order);
+
+        }
+    }
+
+//    public void findZone(Request order){
+//        if (order.getDeliveryLocation() == Zone.WESTL)
+//    }
 }
+
+
